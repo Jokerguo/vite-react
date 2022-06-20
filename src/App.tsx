ba7667e3f389d32f3aeb7fed6e8c1fbfdeba6c1a
guodongsheng <1977036313@qq.com>
2022-06-20 17:50:38 +0800

    引入antd/less、布局、设置主题色

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,61 @@
-import { useState } from "react"
+import { Layout, Menu } from 'antd'
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons'
+import React, { useState } from 'react'
+const { Header, Sider, Content } = Layout
 
-function App() {
-  const [count,setCount] = useState(0)
+const App: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(false)
+  const menuList = [
+    {
+      key: '1',
+      icon: <UserOutlined />,
+      label: '菜单一',
+    },
+    {
+      key: '2',
+      icon: <VideoCameraOutlined />,
+      label: '菜单二',
+    },
+    {
+      key: '3',
+      icon: <UploadOutlined />,
+      label: '菜单三',
+    },
+  ]
 
   return (
     <div className="App">
-      <h1>layout</h1>
-      <div>内容区</div>
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="logo">logo</div>
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            items={menuList}
+          />
+        </Sider>
+        <Layout className="layout-container">
+          <Header className="layout-header">
+            {React.createElement(
+              collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+              {
+                className: 'trigger',
+                onClick: () => setCollapsed(!collapsed),
+              }
+            )}
+          </Header>
+          <Content className="layout-content">
+            Content
+          </Content>
+        </Layout>
+      </Layout>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          // 此处也可设置直角、边框色、字体大小等
+          'primary-color': '#645AFF'
+        },
+        javascriptEnabled: true
+      }
+    }
+  }
 })
